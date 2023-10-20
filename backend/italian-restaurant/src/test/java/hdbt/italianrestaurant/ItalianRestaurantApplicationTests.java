9c81d1333e247671414a1aed740b3fdc5363c5bd
BogdanyTamas <bogdanytamas377@gmail.com>
2023-10-20 18:04:48 +0200

    Backend Frontend start

---
+++ backend/italian-restaurant/src/test/java/hdbt/italianrestaurant/ItalianRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package hdbt.italianrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItalianRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
